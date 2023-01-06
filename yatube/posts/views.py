e38f81fa22a83f0035e685ebce0d473a558debd7
Steplton7 <107725240+Steplton7@users.noreply.github.com>
2023-01-07 00:11:30 +0300

    Complete paginator and page login(form), logout. Start write page password_reset

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,21 +1,45 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 # Импортируем модель, чтобы обратиться к ней
 from .models import Post, Group
+import datetime
+
 # Create your views here.
 SORTING = 10 
 
+def year(request):
+    """Добавляет переменную с текущим годом."""
+    years = datetime.datetime.now()
+    return {
+        'year': years.year
+    }
+    
+years = year(year)
+
 def index(request):
-    # Одна строка вместо тысячи слов на SQL:
-    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
-    posts = Post.objects.order_by('-pub_date')[:SORTING]
-    # В словаре context отправляем информацию в шаблон
+    post_list = Post.objects.all().order_by('-pub_date')
+    # Если порядок сортировки определен в классе Meta модели,
+    # запрос будет выглядеть так:
+    # post_list = Post.objects.all()
+    # Показывать по 10 записей на странице.
+    paginator = Paginator(post_list, 10) 
+
+    # Из URL извлекаем номер запрошенной страницы - это значение параметра page
+    page_number = request.GET.get('page')
+
+    # Получаем набор записей для страницы с запрошенным номером
+    page_obj = paginator.get_page(page_number)
+    # Отдаем в словаре контекста
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context) 
 
 # View-функция для страницы сообщества:
+
+
+@login_required #декоратор для проверки залогиененых пользователей
 def group_posts(request, slug):
     # Функция get_object_or_404 получает по заданным критериям объект 
     # из базы данных или возвращает сообщение об ошибке, если объект не найден.
@@ -26,10 +50,14 @@ def group_posts(request, slug):
     # Метод .filter позволяет ограничить поиск по критериям.
     # Это аналог добавления
     # условия WHERE group_id = {group_id}
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:SORTING]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
-        'posts': posts,
+        #'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context) 
 
--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,7 +1,7 @@
 # users/urls.py
 
 # Импортируем из приложения django.contrib.auth нужный view-класс
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView, PasswordResetCompleteView
 from django.urls import path
 
 from . import views
@@ -19,5 +19,27 @@ urlpatterns = [
         'login/',
         LoginView.as_view(template_name='users/login.html'),
         name='login'
+    ),
+
+    path(
+        'password_change/done/',
+        PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
+        name='password_change_done'
+    ),
+    path(
+        'password_change/',
+        PasswordChangeView.as_view(template_name='users/password_change_form.html'),
+        name='password_change'
+    ),
+
+    path(
+        'password_reset/done',
+        PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+        name='password_reset_complete'
+    ),
+    path(
+        'password_reset/',
+        PasswordResetView.as_view(template_name='users/password_reset_form.html'),
+        name='password_reset_form'
     ),
 ]
