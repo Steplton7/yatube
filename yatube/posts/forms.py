a16b2b3ee4e0538a41e813e7bb77bf7c4bb4d761
Steplton7 <107725240+Steplton7@users.noreply.github.com>
2023-01-11 20:33:12 +0300

    Complete navigation and form edit password

---
+++ yatube/posts/forms.py
@@ -0,0 +1,11 @@
+from django import forms 
+from .models import Post 
+
+class PostForm(forms.ModelForm): 
+    class Meta: 
+        model = Post 
+        labels = { 
+            'group': 'группа', 
+            'text': 'пост', 
+        }
+        fields = ('text', 'group') 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect 
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 # Импортируем модель, чтобы обратиться к ней
-from .models import Post, Group
+from .models import Post, Group, User
+from .forms import PostForm 
 import datetime
 
 # Create your views here.
@@ -11,10 +12,7 @@ SORTING = 10
 def year(request):
     """Добавляет переменную с текущим годом."""
     years = datetime.datetime.now()
-    return {
-        'year': years.year
-    }
-    
+    return years.year
 years = year(year)
 
 def index(request):
@@ -33,6 +31,8 @@ def index(request):
     # Отдаем в словаре контекста
     context = {
         'page_obj': page_obj,
+        'years': years
+        
     }
     return render(request, 'posts/index.html', context) 
 
@@ -58,9 +58,79 @@ def group_posts(request, slug):
         'group': group,
         #'posts': posts,
         'page_obj': page_obj,
+        'years': years
     }
-    return render(request, 'posts/group_list.html', context) 
+    return render(request, 'posts/group_list.html', context)
 
+
+def profile(request, username):
+    # Здесь код запроса к модели и создание словаря контекста
+    #author = get_object_or_404(Post, author=username)
+    author = get_object_or_404(User, username=username) 
+    posts = Post.objects.filter(author=author.id).order_by('-pub_date')
+    #  page_obj = get_paginator(request, posts) 
+    #posts = Post.objects.filter(author=username).order_by('-pub_date')
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'author': author,
+        'posts': posts,
+        'page_obj': page_obj,
+        'years': years
+    }
+    return render(request, 'posts/profile.html', context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id) 
+    # Здесь код запроса к модели и создание словаря контекста
+    context = {
+        'post': post,
+        'years': years
+    }
+    return render(request, 'posts/post_detail.html', context)
+
+
+@login_required 
+def post_create(request): 
+    form = PostForm(request.POST, files=request.FILES or None) 
+    if request.method == 'POST': 
+
+        if form.is_valid(): 
+            post = form.save(commit=False) 
+            post.author = request.user 
+            post.save() 
+            return redirect('posts:profile', request.user.username) 
+    # form = PostForm() 
+    template = 'posts/create_post.html' 
+    context = { 
+        'form': form,
+        'years': years
+    } 
+    return render(request, template, context) 
+
+
+@login_required 
+def post_edit(request, post_id): 
+    post = get_object_or_404(Post, id=post_id) 
+    form = PostForm(request.POST or None, instance=post)
+
+    if request.user != post.author: 
+        return redirect('posts:post_detail', post_id) 
+
+    if form.is_valid(): 
+        form.save() 
+        return redirect('posts:post_detail', post_id) 
+
+    context = { 
+        'form': form, 
+        'is_edit': True, 
+        'post': post,
+        'years': years
+    } 
+
+    return render(request, 'posts/create_post.html', context)
 """
 def group_list(request):
     template = 'posts/group_list.html'
